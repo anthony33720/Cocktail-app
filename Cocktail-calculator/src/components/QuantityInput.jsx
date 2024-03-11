175896ef2bd66dbb4146a638a0c2b90668f11256
Anthony33720 <raynaud.anthony@gmail.com>
2024-03-11 12:37:01 +0100

    quantity input

---
+++ Cocktail-calculator/src/components/QuantityInput.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const QuantityInput = ({ value, onChange }) => {
+  return (
+    <div>
+      <label htmlFor="quantity">Quantity (cl): </label>
+      <input
+        type="number"
+        id="quantity"
+        name="quantity"
+        value={value}
+        onChange={onChange}
+        min="0"
+        step="1"
+      />
+    </div>
+  );
+};
+
+export default QuantityInput;
